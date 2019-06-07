d511a1f7d9b2000c0f5c07f99af13a115cbcb278
Oran Nahoum <33873115+oranichu@users.noreply.github.com>
2019-06-07 16:27:51 +0300

    testing malloc_1

---
+++ malloc_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unistd.h>
+
+#define MAX_MALLOC_SIZE 100000000
+void* malloc(size_t size) {
+
+    if (size == 0 || size > MAX_MALLOC_SIZE) {
+        return NULL;
+    }
+    void*  increment = &size;
+    void* prev_program_break = sbrk(increment);
+
+    if (*(int*)prev_program_break < 0) {
+        return NULL;
+    }
+
+    return prev_program_break;
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
